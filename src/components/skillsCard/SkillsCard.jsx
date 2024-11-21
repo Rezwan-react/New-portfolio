91214e73859b45f9c5586ea86930f26f0523cd57
Rezwan-react <mdrazwanislam8@gmail.com>
2024-11-22 00:05:58 +0600

    added Home, About,Skills Response

--- src/components/skillsCard/SkillsCard.jsx
+++ src/components/skillsCard/SkillsCard.jsx
@@ -5,7 +5,7 @@ function SkillsCard({skills_icons, skills_text}) {
     <>
             <div 
             data-text="GitHub"
-            className=" relative w-[180px] h-[200px] border border-white/10 shadow-[0px_25px_25px_rgba(0,0,0,0.25)] flex justify-center items-center transition-transform duration-500 rounded-lg mx-4 backdrop-blur-md hover:scale-105 transition duration-300 ease-in-out"
+            className="  relative w-[180px] h-[200px] border border-white/10 shadow-[0px_25px_25px_rgba(0,0,0,0.25)] flex justify-center items-center  rounded-lg mx-4 backdrop-blur-md hover:scale-105 transition duration-300 ease-in-out"
             style={{
               background: 'linear-gradient(rgba(255, 255, 255, 0.2), transparent) ', // Custom gradient background
             }}
@@ -14,7 +14,7 @@ function SkillsCard({skills_icons, skills_text}) {
 
                      {skills_icons}
                 </div>
-            <div className="absolute bottom-0 w-full h-10 bg-white/5 flex justify-center items-center text-white ">
+            <div className="absolute bottom-0 w-full h-10 bg-white/5 flex justify-center items-center text-white  ">
               {skills_text}
             </div>
           </div>
