8911fd3b6585ee89f49614cb1eb33665a9036254
Rezwan-react <mdrazwanislam8@gmail.com>
2025-04-23 22:50:45 +0600

    Calculation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,25 +4,26 @@ import PortfolioCard from '../portfolioCard/PortfolioCard'
 function Portfolio() {
   return (
     <>
-    <section id='portfolio'>
-      <div className="motion portfolio ">
-        <div className="container">
-          <div className="portfolio_row">
-          <div className="portfolio_head">
+      <section id='portfolio'>
+        <div className="motion portfolio ">
+          <div className="container">
+            <div className="portfolio_row">
+              <div className="portfolio_head">
                 <h2>Portfolio</h2>
               </div>
-            <div className="portfolio_col">
-              <PortfolioCard portfolio_img= 'images/Mixedit-UP.png' portfolio_head='Mixedit-UP' portfolio_text='Mixedit-UP is a modern food planning website that provides categorized meal options for different times of the day: Future Breakfast, Lunch, Evening, and Dinner. This project is built using React and Tailwind CSS.' Live_link ="https://mixedit-up-three.vercel.app/" />
-              <PortfolioCard portfolio_img= 'images/Calculator.png' portfolio_head='Calculator' portfolio_text='This project is a basic calculator web application built using HTML, CSS, and JavaScript. The calculator supports basic arithmetic operations such as addition, subtraction, multiplication, and division. This documentation outlines the key features, structure, and code used to build the website.' Live_link ="https://calculator-one-beryl-33.vercel.app/" />
-              <PortfolioCard portfolio_img= 'images/Furniture.png' portfolio_head='Furniture' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' />
-              <PortfolioCard portfolio_img= 'images/numta.png' portfolio_head='Numta' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' />
-              <PortfolioCard portfolio_img= 'images/fist.png' portfolio_head='Portfolio' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' />
-              <PortfolioCard portfolio_img= 'images/create.png' portfolio_head='create' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' />
+              <div className="portfolio_col">
+                <PortfolioCard portfolio_img='images/Mixedit-UP.png' portfolio_head='Mixedit-UP' portfolio_text='Mixedit-UP is a modern food planning website that provides categorized meal options for different times of the day: Future Breakfast, Lunch, Evening, and Dinner. This project is built using React and Tailwind CSS.' Live_link="https://mixedit-up-three.vercel.app/" />
+                <PortfolioCard portfolio_img='images/Calculation .png' portfolio_head='Calculator' portfolio_text='Title: Calculator Web App
+                  Description: A simple yet modern calculator built with HTML, CSS, and JavaScript. The project demonstrates basic front-end development skills with an interactive and visually appealing design.' Live_link="https://new-calculator-orpin.vercel.app/" />
+                <PortfolioCard portfolio_img='images/Furniture.png' portfolio_head='Furniture' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' />
+                <PortfolioCard portfolio_img='images/numta.png' portfolio_head='Numta' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' Live_link="https://numta-rezwan-reacts-projects.vercel.app/" />
+                <PortfolioCard portfolio_img='images/fist.png' portfolio_head='Portfolio' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' />
+                <PortfolioCard portfolio_img='images/create.png' portfolio_head='create' portfolio_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.' />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>  
+      </section>
     </>
   )
 }
